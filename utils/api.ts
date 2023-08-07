2d4c60b6e35b5b0a0bdda371693e4531a58bc1ec
sundesoleil <sunaimeles@gmail.com>
2023-08-07 13:52:50 +0900

    오타를,,수정

--- utils/api.ts
+++ utils/api.ts
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URI,
 })
-
-console.log('# BASE URL =>', api.defaults.baseURL)
 
 export default api
